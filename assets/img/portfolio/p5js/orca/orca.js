2e2ffec702feaf44a65b9a20b11ae416e77b3d84
Jaume Albardaner i Torras <jaal5534@colorado.edu>
2024-06-01 15:07:34 +0200

    Fixed backward tail and added alpha channel

--- assets/img/portfolio/p5js/orca/orca.js
+++ assets/img/portfolio/p5js/orca/orca.js
@@ -1,55 +1,64 @@
-// Shape of the object is a circle 
-let ball; 
-
 let yOffset = -100; // Adjust this value to set the vertical offset
 let weight = 0.1;
 
-let images = []; // Array to store images
 let totalImages = 44; // Number of images to load
+let images = new Array(totalImages); // Array to store images
+let trans_images = new Array(totalImages); // Array to store images
 
-let pastMousePositions = []; // Array to store past mouse positions
-let pastOrcaPositions = []; // Array to store past orca positions
+let pastMousePositions = new Array(totalImages); // Array to store past mouse positions
+let pastOrcaPositions = new Array(totalImages); // Array to store past orca positions
 
-let toggleBoolean = true; // Boolean variable to be toggled
+let backwardsBoolean = true; // Boolean variable to be toggled
+let alphaBoolean = false; // Boolean variable to be toggled
 let bKeyPressed = false; // Flag to track "B" key press
+let aKeyPressed = false; // Flag to track "B" key press
 
 let maxDistance = 120; // Maximum distance to cover per iteration
+
 
 function preload() {
   for (let i = 1; i <= totalImages; i++) {
     let imgPath = "" + i + ".png";
-    images.push(loadImage(imgPath));
+    images[i-1] = (loadImage(imgPath));
+  }
+  for (let i = 1; i <= totalImages; i++) {
+    let imgPath = "../orca_trans/" + i + ".png";
+    trans_images[i-1] = (loadImage(imgPath));
   }
 }
 
 // This method is used for creating canvas 
 function setup() { 
     createCanvas(windowWidth-20, windowHeight-20); 
-    // frameRate(10); // Set the frame rate to 10 frames per second
+    background(255); 
 
     // Preload screen center
     // Store current mouse position
-    while (pastMousePositions.length < totalImages) {
+    for (let i = 0; i < totalImages; i++) {
       let screenCenter = createVector((windowWidth-20)/2, (windowHeight-20)/2);
-      pastMousePositions.push(screenCenter);
-    }
-    // Set current orca positions to center
-    while (pastOrcaPositions.length < totalImages) {
-      let screenCenter = createVector((windowWidth-20)/2, (windowHeight-20)/2);
-      pastOrcaPositions.push(screenCenter);
+      pastMousePositions[i] = (screenCenter);
+      pastOrcaPositions[i] = (screenCenter);
     }
 } 
 
 // This method will draw the circle  
 // and track the mouse as well. 
 function draw() { 
+    clear()
 
     // Check if the "B" key is pressed
     if (keyIsDown(66) && !bKeyPressed) { // ASCII code for "B"
-      toggleBoolean = !toggleBoolean; // Toggle between true and false
+      backwardsBoolean = !backwardsBoolean; // Toggle between true and false
       bKeyPressed = true; // Set the flag to true
     } else if (!keyIsDown(66)) {
       bKeyPressed = false; // Reset the flag when the "B" key is released
+    }
+    // Check if the "A" key is pressed
+    if (keyIsDown(65) && !aKeyPressed) { // ASCII code for "a"
+      alphaBoolean = !alphaBoolean; // Toggle between true and false
+      aKeyPressed = true; // Set the flag to true
+    } else if (!keyIsDown(65)) {
+      aKeyPressed = false; // Reset the flag when the "a" key is released
     }
     
     // Store current mouse position
@@ -58,14 +67,13 @@ function draw() {
     pastMousePositions.shift(); // Remove the oldest position
 
 
-    background(255); 
     let deltaX = 0;
     let deltaY = 0;
     let distance = 0;
-    fill(0); 
     
     for (let i = 1; i <= totalImages; i++) {
-      let j = toggleBoolean*(totalImages-i)+ !toggleBoolean*(i-1);
+      let j = backwardsBoolean*(totalImages-i)+ !backwardsBoolean*(i-1);
+      let i_aux = !backwardsBoolean*(totalImages-i)+ backwardsBoolean*(i-1);
       deltaX = - pastOrcaPositions[i-1].x + pastMousePositions[i-1].x;
       deltaY = - pastOrcaPositions[i-1].y + pastMousePositions[i-1].y;
       distance = dist(pastOrcaPositions[i-1].x, pastOrcaPositions[i-1].y, pastMousePositions[i-1].x, pastMousePositions[i-1].y);
@@ -77,8 +85,16 @@ function draw() {
         pastOrcaPositions[i-1].x += deltaX * weight;
         pastOrcaPositions[i-1].y += deltaY * weight;
       }
-
-      image(images[j], pastOrcaPositions[i-1].x-images[i-1].width/2,
-        pastOrcaPositions[i-1].y-images[i-1].height/2 + yOffset);
-    }    
+      
+      if(alphaBoolean)
+      {
+        image(trans_images[j], pastOrcaPositions[i_aux].x-images[i_aux].width/2,
+        pastOrcaPositions[i_aux].y-images[i_aux].height/2 + yOffset);
+      }
+      else
+      {
+        image(images[j], pastOrcaPositions[i_aux].x-images[i_aux].width/2,
+          pastOrcaPositions[i_aux].y-images[i_aux].height/2 + yOffset);
+      }
+  }    
 } 
